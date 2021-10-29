01063e51724054c9d47ba7b10a63e2cf1b2eefc0
Nicolas Kaiser <nikolas.luis@gmail.com>
2021-10-29 17:28:02 -0300

    cadastro js

--- js/jscadastro.js
+++ js/jscadastro.js
@@ -2,17 +2,50 @@
 (Função Procurar e Função CriarConta)
 */
 
+usuarios=[];
+
 function pag_login01() {
-	document.getElementById("img_login").value
-	location.href = "http://localhost/vinilandia/pag/login.html"
+	document.getElementById("img_login").value;
+	location.href = "http://localhost/vinilandia/pag/login.html";
 }
 
 function pag_sacola() {
-	document.getElementById("img_sacola").value
-	location.href = "http://localhost/vinilandia/pag/sacola-de-compras.html"
+	document.getElementById("img_sacola").value;
+	location.href = "http://localhost/vinilandia/pag/sacola-de-compras.html";
 }
 
 function pag_login02() {
-	document.getElementById("b_login").value
-	location.href = "http://localhost/vinilandia/pag/login.html"
+	document.getElementById("b_login").value;
+	location.href = "http://localhost/vinilandia/pag/login.html";
+}
+
+var usuarios = [];
+
+function cadastrar(){
+	var aux=[];
+
+	var usuario = document.getElementById("usuario").value;
+	var email = document.getElementById("email").value;
+	var senha = document.getElementById("senha").value;
+	var confirmarSenha = document.getElementById("confirmarSenha").value;
+
+	if(senha!=confirmarSenha){
+		document.getElementById("senha").innerHTML += "<div> senhas erradas man </div>";
+		senhazinha.confirmarSenha.focus();
+		return false;
+	}
+
+	aux.push(usuario);
+	aux.push(email);
+	aux.push(senha);
+	alert("cadastro realizado");
+
+	usuarios.push("aux");
+}
+
+function senhas(){
+
+	document.getElementById("senha").innerHTML = " ";
+	
+
 }
